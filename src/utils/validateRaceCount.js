6d0bd68b264f77a2c4aea980a47f9c6610d4e103
mimizae <jim400934@gmail.com>
2025-10-25 18:56:33 +0900

    feat: 시도할 횟수 검증 함수 구현

--- src/utils/validateRaceCount.js
+++ src/utils/validateRaceCount.js
@@ -1,1 +1,11 @@
-export function validateRaceCount(raceCount = 0) {}
+export function validateRaceCount(raceCount = 0) {
+  if (raceCount == "") {
+    throw new Error("[ERROR]: 시도할 횟수를 입력해 주세요.");
+  }
+  if (typeof raceCount != "number") {
+    throw new Error("[ERROR]: 시도할 횟수에는 숫자만 입력 가능합니다.");
+  }
+  if (raceCount <= 0) {
+    throw new Error("[ERROR]: 시도할 횟수는 1 이상의 자연수여야 합니다.");
+  }
+}
