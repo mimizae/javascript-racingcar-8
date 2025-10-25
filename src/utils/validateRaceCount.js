2ac0a788a9d7dec73b05a5e5975087e912ab0f9e
mimizae <jim400934@gmail.com>
2025-10-25 20:07:17 +0900

    chore: 오타 수정

--- src/utils/validateRaceCount.js
+++ src/utils/validateRaceCount.js
@@ -10,7 +10,7 @@ export function validateRaceCount(raceCount = 0) {
     throw new Error("[ERROR]: 시도할 횟수에는 숫자만 입력 가능합니다.");
   }
 
-  if (!Number.isInteger(count) || count <= 0) {
+  if (!Number.isInteger(raceCount) || raceCount <= 0) {
     throw new Error("[ERROR]: 시도할 횟수는 1 이상의 자연수여야 합니다.");
   } // 단순히 음수 구분이 아니라 자연수인지 아닌지를 구분
 }
