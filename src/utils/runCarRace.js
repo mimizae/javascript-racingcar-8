098511e0b62a249914e93d930629cc730bd76021
mimizae <jim400934@gmail.com>
2025-10-25 19:22:20 +0900

    feat: 차수별 각 자동차의 전진 횟수 출력 로직 작성

--- src/utils/runCarRace.js
+++ src/utils/runCarRace.js
@@ -1,4 +1,20 @@
 export function runCarRace(carNames = "", raceCount = 0) {
   let carNameList = carNames.split(",");
-  while (raceCount) {}
+  // 차가 몇 개인지는 carNameList.length로 알 수 있다.
+  // 각 자동차 별 차수 동안 진행된 전진 횟수를 저장할 공간 필요. 초기화는 0
+
+  let progress = Array(carNameList.length).fill(0);
+
+  for (let i = 0; i < raceCount; i++) {
+    carNameList.forEach((carName) => {
+      const randomNum = Math.floor(Math.random() * 10);
+      if (randomNum >= 4) progress[carName] += 1;
+    });
+
+    // 진행 상황 출력
+    carNameList.forEach((name) => {
+      MissionUtils.Console.print(`${name} : ${"-".repeat(progress[name])}`);
+    });
+    MissionUtils.Console.print(""); // 출력마다 줄 바꿈
+  }
 }
