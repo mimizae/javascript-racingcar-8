24d92918cad2fdbd13a39a82ff4369b9bc0c338a
mimizae <jim400934@gmail.com>
2025-10-27 00:04:10 +0900

    refactor: 경주 실행함수의 내부 로직들을 단일 함수 원칙에 따라 분리

--- src/utils/runCarRace.js
+++ src/utils/runCarRace.js
@@ -1,35 +1,59 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 export function runCarRace(validatedCarNameList = [], validatedRaceCount = 0) {
-  const carMoveCountMap = {};
-  validatedCarNameList.forEach((carName) => (carMoveCountMap[carName] = 0)); // 각 자동차 이름을 key로 사용해 일반 객체로 활용
+  const carMoveCountMap = initializeCarMoveCount(validatedCarNameList);
 
   MissionUtils.Console.print("\n실행 결과\n");
 
   for (let i = 0; i < validatedRaceCount; i++) {
-    validatedCarNameList.forEach((carName) => {
-      const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
-      if (randomNum >= 4) carMoveCountMap[carName] += 1;
-    });
-
-    validatedCarNameList.forEach((carName) => {
-      MissionUtils.Console.print(
-        `${carName} : ${"-".repeat(carMoveCountMap[carName])}`
-      );
-    });
-    MissionUtils.Console.print("");
+    playRace(validatedCarNameList, carMoveCountMap);
+    printRaceResult(validatedCarNameList, carMoveCountMap);
   }
 
+  const raceWinners = determineRaceWinners(carMoveCountMap);
+  printRaceWinners(raceWinners);
+}
+
+// 자동차별 전진 거리 초기화
+function initializeCarMoveCount(validatedCarNameList) {
+  const carMoveCountMap = {};
+  validatedCarNameList.forEach((carName) => (carMoveCountMap[carName] = 0)); // 각 자동차 이름을 key로 사용해 일반 객체로 활용
+  return carMoveCountMap;
+}
+
+// 한 레이스 진행
+function playRace(validatedCarNameList, carMoveCountMap) {
+  validatedCarNameList.forEach((carName) => {
+    const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (randomNum >= 4) carMoveCountMap[carName] += 1;
+  });
+}
+
+// 레이스 결과 출력
+function printRaceResult(validatedCarNameList, carMoveCountMap) {
+  validatedCarNameList.forEach((carName) => {
+    MissionUtils.Console.print(
+      `${carName} : ${"-".repeat(carMoveCountMap[carName])}`
+    );
+  });
+  MissionUtils.Console.print("");
+}
+
+// 최종 우승자 계산
+function determineRaceWinners(carMoveCountMap) {
   const maxDistance = Math.max(...Object.values(carMoveCountMap));
 
-  if (maxDistance === 0) {
+  if (maxDistance === 0) return [];
+  return Object.entries(carMoveCountMap)
+    .filter(([_, distance]) => distance === maxDistance)
+    .map(([carName]) => carName);
+}
+
+// 우승자 출력
+function printRaceWinners(raceWinners) {
+  if (raceWinners.length === 0) {
     MissionUtils.Console.print("최종 우승자 : 우승자는 없습니다.");
-    return;
+  } else {
+    MissionUtils.Console.print(`최종 우승자 : ${raceWinners.join(", ")}`);
   }
-
-  const raceWinners = validatedCarNameList.filter(
-    (carName) => carMoveCountMap[carName] === maxDistance
-  );
-
-  MissionUtils.Console.print(`최종 우승자 : ${raceWinners.join(", ")}`);
 }
